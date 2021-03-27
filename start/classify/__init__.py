88e3f0553f9b00af1fef07f67797e45686b5f74c
Ammar <ammarraneez@gmail.com>
2021-03-28 01:25:03 +0530

    set image content type for online image rendering and return image url in response for frontend

--- start/classify/__init__.py
+++ start/classify/__init__.py
@@ -4,9 +4,10 @@ import numpy as np
 import azure.functions as func
 import cv2
 import json
+import uuid
 from .app import upload
 from werkzeug.utils import secure_filename
-from azure.storage.blob import BlobServiceClient, BlobClient
+from azure.storage.blob import BlobServiceClient, BlobClient, ContentSettings
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     headers = {
@@ -17,14 +18,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     text_test = req.params.get("name")
 
     f = req.files['file']
-    filename = f.filename
+    filename = str(uuid.uuid4()) + ".jpg"
     filestream = f.stream
     filestream.seek(0)
 
     # save image to azure storage blob
     try:
         blob = BlobClient.from_connection_string(conn_str= "DefaultEndpointsProtocol=https;AccountName=bisfyp;AccountKey=+NYIa2OlyIc/sPLMSeaSeWWU70o2CD8YX128gdbyG184M7F2EDuRDmilCmYOIowk/9/GmioI2FiE0hE+N9atnw==", container_name="images", blob_name=filename)
-        blob.upload_blob(filestream.read(), blob_type="BlockBlob")
+        cnt_settings = ContentSettings(content_type="image/jpeg")
+        blob.upload_blob(filestream.read(), blob_type="BlockBlob", content_settings=cnt_settings)
     except:                                                                                                                                                                          
         pass
 
@@ -35,5 +37,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         x = blob_data.content_as_bytes()
         x = np.fromstring(x, dtype='uint8')
         results = upload(x)
-        return func.HttpResponse(json.dumps(results), headers = headers)
+        return func.HttpResponse(json.dumps([{"result": results, "image_url": f"https://bisfyp.blob.core.windows.net/images/{filename}"}]), headers = headers)
 
